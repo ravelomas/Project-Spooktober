c85d71468c036079cfe88571d23417f5327a383f
roryrothon <32644161+roryrothon@users.noreply.github.com>
2017-10-09 16:17:48 +0100

    .

--- Rothon/lootSpawn/lootArray.hpp
+++
@@ -1,6 +0,0 @@
-//=======================================================================================
-//	LOOT SPAWN SYSTEM FOR THE SPOOKTOBER FREAK SHOW =====================================
-//=======================================================================================
-
-// All primary weapons into array
-_weaponArray = "getNumber( _x >> 'scope' ) isEqualTo 2 && { getNumber( _x >> 'type' ) isEqualTo 1 }"configClasses( configFile >> "CfgWeapons" ) apply { configName _x };
