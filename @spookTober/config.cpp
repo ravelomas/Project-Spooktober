4332d4f264a155bd251cdbe0c49221107cf82598
roryrothon <32644161+roryrothon@users.noreply.github.com>
2017-10-09 16:27:06 +0100

    Update config.cpp

---
+++ @spookTober/config.cpp
@@ -0,0 +1,28 @@
+class CfgPatches {
+
+  class spookTober {
+  
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = 0.1;
+    requiredAddons[] = {};
+  
+  };
+
+};
+
+class functions {
+
+  class spookTober {
+  
+    class spookTober_lootSpawn {};
+    
+    class spookTober_zombieSpawn {};
+    
+    class spookTober_randoimSpawn {};
+    
+    class spookTober_respawn {};
+  
+  };
+
+};
